8d012cfcc8180d56ee1a045bf884eefda37604ed
Jasur7674 <jasur7674@mail.ru>
2024-11-15 00:05:21 +0900

    feat: develope ejs page

--- server.js
+++ server.js
@@ -2,44 +2,40 @@ console.log("starting web server");
 const express = require ("express");
 const app = express();
 const http = require('http');
+const fs = require("fs");
+const { error } = require("console");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1 - Entry codes
-
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
 // 2 - session codes
-
-
 // 3 - views codes
-
 app.set("views", "views");
 app.set("view engine", "ejs");
 
-
 // 4 - rooting codes
-
-// app.get("/hello", function (req, res) {
-//     res.end(`<h1 style= "background: orange" >"Hello world!"</h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//     res.end(`<h1 style= "background: orange" >"You are at the page of gifts"</h1>`);
-// });
-
-
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-    res.json({test:"succesfully woring..."});
+    // console.log(req.body);
+    // res.json({test:"succesfully woring..."});
+});
+app.get("/author", (req, res) => {
+    res.render("author", {user: user });
 });
 
-app.get('/', function(req, res) {
-    res.render('shopping');
+app.get("/", function(req, res) {
+    res.render("shopping");
 });
-
-
 
 const server = http.createServer(app);
 let PORT = 3000;
@@ -47,8 +43,6 @@ server.listen(PORT,  function() {
     console.log(`Server is running succesfully on port: ${PORT}`);
 });
 
-// There are 2 ways to make web(frontend): traditional:using ejs-framework(in Backend building frontend) BSSR- backend server side rendering.
-// (2 modern : single page aplication (react))
 
 
 
