86388e3f7fb93fc62a37f9c23a38dda66583c6c9
Jasur7674 <jasur7674@mail.ru>
2024-11-15 21:33:50 +0900

    feat:porfolio page publish

--- server.js
+++ server.js
@@ -8,24 +8,24 @@ const { error } = require("console");
 let user;
 fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
-        console.log("ERROR:", err);
+        console.log("ERROR:", err); 
     } else {
-        user = JSON.parse(data) 
-    }
-});
+        user = JSON.parse(data)  
+    } 
+});     
 
-// 1 - Entry codes
+// 1 - Entry codes      
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true})); 
 
-// 2 - session codes
-// 3 - views codes
-app.set("views", "views");            
-app.set("view engine", "ejs");      
+// 2 - session codes    
+// 3 - views codes  
+app.set("views", "views");               
+app.set("view engine", "ejs");        
 
 // 4 - rooting codes
-app.post("/create-item", (req, res) => { 
+app.post("/create-item", (req, res) => {   
     // console.log(req.body);
     // res.json({test:"succesfully woring..."});
 });
