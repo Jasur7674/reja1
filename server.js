7a24cb40af57581e90235e9977eebd298431803a
Jasur7674 <jasur7674@mail.ru>
2024-11-16 16:33:22 +0900

    feat: portfolio page publish

--- server.js
+++ server.js
@@ -10,29 +10,29 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
     if(err) {
         console.log("ERROR:", err); 
     } else {
-        user = JSON.parse(data)  
-    } 
-});     
-
-// 1 - Entry codes      
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true})); 
-
-// 2 - session codes    
-// 3 - views codes  
-app.set("views", "views");               
-app.set("view engine", "ejs");        
+        user = JSON.parse(data)      
+    }   
+});         
+ 
+// 1 - Entry codes        
+app.use(express.static("public"));   
+app.use(express.json());  
+app.use(express.urlencoded({extended: true}));    
+  
+// 2 - session codes     
+// 3 - views codes   
+app.set("views", "views");                         
+app.set("view engine", "ejs");             
 
 // 4 - rooting codes
 app.post("/create-item", (req, res) => {   
-    // console.log(req.body);
-    // res.json({test:"succesfully woring..."});
-});
+    // console.log(req.body); 
+    // res.json({test:"succesfully woring..."});     
+}); 
 app.get("/author", (req, res) => {
-    res.render("author", {user: user });
-});
-
+    res.render("author", {user: user });   
+});  
+ 
 app.get("/", function(req, res) {
     res.render("shopping");
 });
